46613d018793a475b0b09cec94150a78add4a5eb
Dzmitry Akulich <dzmitry.akulich.33@gmail.com>
2022-10-01 10:03:55 +0300

    Updates

---
+++ index.69167065.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".testimonials-slider").slick({dots:!0,speed:1e3})}));
+//# sourceMappingURL=index.69167065.js.map
---
+++ index.f6b5b233.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".testimonials-slider").slick({dots:!0,speed:1e3})}));
+//# sourceMappingURL=index.f6b5b233.js.map
